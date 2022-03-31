47ebaecbdb40cb5afd7646d7464fbb86c287cdf2
unknown <namcam266@gmail.com>
2022-04-01 01:25:31 +0700

    swab

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-
+    do_Ph = request.args.get('do_Ph')
+    nhiet_do = request.args.get('nhiet_do')
+    do_duc = request.args.get('do_duc')
+    return render_template('index.html', do_Ph=do_Ph, nhiet_do=nhiet_do, do_duc=do_duc)
 
 if __name__ == "__main__":
     app.run(debug=True) 
